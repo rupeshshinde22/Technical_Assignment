b6522c2ace0536737d5596ae32558b557b1eb18c
rupeshshinde22 <rupesh.shinde@springernature.com>
2026-08-11 23:37:09 +0530

    adding product management api

---
+++ product-management-api/src/main/kotlin/com/aigp/product_management_api/ProductManagementApiApplication.kt
@@ -0,0 +1,11 @@
+package com.aigp.product_management_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductManagementApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductManagementApiApplication>(*args)
+}
---
+++ product-management-api/src/test/kotlin/com/aigp/product_management_api/ProductManagementApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aigp.product_management_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductManagementApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
