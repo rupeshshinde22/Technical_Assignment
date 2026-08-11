8d5019c2f70c1cc7e7f62e6c31bb2ab7fc65496f
rupeshshinde22 <rupesh.shinde@springernature.com>
2026-08-12 02:52:54 +0530

    gradle build updated

--- product-management-api/build.gradle.kts
+++ product-management-api/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("jvm") version "2.3.21"
-	kotlin("plugin.spring") version "2.3.21"
+	kotlin("jvm") version "2.0.21"
+	kotlin("plugin.spring") version "2.0.21"
 	id("org.springframework.boot") version "4.1.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -32,6 +32,7 @@ dependencies {
 
 kotlin {
 	compilerOptions {
+		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
 		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
 	}
 }
